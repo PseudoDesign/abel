436c37cec1143387eab718073090504087215b58
adam <adam@m-85.com>
2016-11-10 22:59:05 -0700

    Reworked Crest tests

--- evelib/tests/test_Crest.py
+++ evelib/tests/test_Crest.py
@@ -2,13 +2,23 @@ import unittest
 from evelib.Crest import CrestConnection
 
 
-class TestCrestConnection(unittest.TestCase):
-    def test_crest_connection(self):
-        eve = CrestConnection()
-        self.assertIsNotNone(eve._data)
+class TestCrestObjectBase(unittest.TestCase):
+    TESTS = {}
+    TEST_OBJECT = None
+    _connection = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls._connection = CrestConnection()
+
+    def test_run_tests(self):
+        """tests format: {test_method : [kwargs, dict(name='derp')]}"""
+        for test in self.TESTS.keys():
+            for kwargs in self.TESTS[test]:
+                test(self, **kwargs)
 
 
-class TestCrestObjectInterface(unittest.TestCase):
+class TestCrestObject(TestCrestObjectBase):
     def test_get_by_attr_value(self):
         # TODO: Write this test
         pass
@@ -16,3 +26,7 @@ class TestCrestObjectInterface(unittest.TestCase):
     def test_get_all_items(self):
         # TODO: Write this test
         pass
+
+    def test_crest_connection(self):
+        eve = CrestConnection()
+        self.assertIsNotNone(eve._data)
