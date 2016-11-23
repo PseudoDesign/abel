66b41f8143d63e062d7eb34b1876b168a80563cd
adam <adam@m-85.com>
2016-11-22 22:50:36 -0700

    Added test for region_id

--- evelib/objects/tests/test_Constellation.py
+++ evelib/objects/tests/test_Constellation.py
@@ -1,5 +1,6 @@
 from evelib.tests.test_Sql import TestSqlObjectBase
 from evelib.objects.Constellation import Constellation
+from evelib.objects.Region import Region
 from evelib.objects.tests.test_CrestSqlHelper import TestCrestSqlInterface
 
 
@@ -10,4 +11,11 @@ class TestConstellation(TestSqlObjectBase, TestCrestSqlInterface):
         "Z6T6-B"
     ]
 
-
+    def test_region_references_constellation(self):
+        TEST_ITEM = self.get_sample_object_name()
+        crest_item = Constellation.get_crest_item_by_attr(self.eve, "name", TEST_ITEM)
+        db_item = self.TEST_OBJECT.create_from_crest_data(crest_item, write=True)
+        region = Region.get_from_db_by_id(db_item.region_id)
+        self.assertEqual(region.id, db_item.region_id)
+        self.assertEqual(region.name, db_item.r_region.name)
+        self.assertEqual(region.name, db_item.r_region.name)
