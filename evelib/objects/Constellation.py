3e64fd317edcfedf0e7f18e353f138a3ae8f750e
adam <adam@m-85.com>
2016-11-22 19:53:28 -0700

    Basic constellation complete

--- evelib/objects/Constellation.py
+++ evelib/objects/Constellation.py
@@ -11,10 +11,18 @@ class Constellation(SqlBase, CrestSqlInterface):
 
     @classmethod
     def get_objects_from_crest(cls, crest_connection):
-        return crest_connection.constellations
+        return crest_connection.constellations().items
 
     @classmethod
     def get_crest_item_by_attr(cls, crest_connection, attr, value):
-        crest_item = crest_connection.get_by_attr_value(
-            crest_connection.get_entries_in_page(cls.get_objects_from_crest(crest_connection)), attr, value)
+        crest_item = crest_connection.get_by_attr_value(cls.get_objects_from_crest(crest_connection), attr, value)
         return crest_item
+
+    @classmethod
+    def get_db_item_by_crest_item(cls, crest_item, **kwargs):
+        retval = cls.get_from_db_by_id(crest_item.id)
+        if retval is None:
+            if 'create_if_null' in kwargs:
+                if kwargs['create_if_null']:
+                    retval = cls.create_from_crest_data(crest_item, **kwargs)
+        return retval
