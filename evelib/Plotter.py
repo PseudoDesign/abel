d36640a67d10cfa8bd6b6024e6760289fb76eeaa
PseudoDesign <PseudoDesign@users.noreply.github.com>
2016-12-01 13:17:49 -0700

    Checkin before refactor

--- evelib/Plotter.py
+++ evelib/Plotter.py
@@ -65,27 +65,21 @@ class Plotter:
 
     @staticmethod
     def draw_data_set(data_set):
-        first = True
         units = data_set.get_entries_by_units()
         loc = len(units)
         for unit in units:
-            if first:
-                first = False
-                fig, x = plt.subplots()
-                ax1 = x
-                ax1.set_xlabel(data_set.x_data.units)
-                linestyle = 'solid'
-            else:
-                x = ax1.twinx()
-                linestyle = 'dashed'
+            fig, x = plt.subplots()
+            ax1 = x
+            ax1.set_xlabel(data_set.x_data.units)
             for_legend = []
             for entry in units[unit]:
-                line, = x.plot(data_set.x_data, data_set[entry], label=data_set[entry].name, linestyle=linestyle)
+                line, = x.plot(data_set.x_data, data_set[entry], label=data_set[entry].name)
                 x.set_ylabel(data_set[entry].units)
                 for_legend += [line]
-            plt.legend(handles=for_legend, loc=loc)
+            plt.legend(handles=for_legend)
             loc -= 1
             lim = x.get_ylim()
             x.set_ylim(lim[0], lim[1]*1.2)
-        plt.title(data_set.get_title())
+            plt.title(data_set.get_title())
+            fig.show()
         plt.show()
