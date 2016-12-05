4f388d93b4d6a955cbd5d999d58ae54b91c36a55
Adam <adam@packer-debian-8-amd64.droplet.local>
2016-12-05 06:05:37 +0000

    Added pymysql connector

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -20,7 +20,7 @@ apt-get update
 apt-get install -y python3-pip
 pip3 install git+git://github.com/pycrest/PyCrest.git
 #pip3 install matplotlib
-pip3 install sqlalchemy pyaml
+pip3 install sqlalchemy pyaml pymysql
 
 # Install MariaDB (debian jessie)
 apt-get install -y software-properties-common
