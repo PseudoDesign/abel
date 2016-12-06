40e924fc58f3ebcddadaa252591db0fe64352842
adam <adam@m-85.com>
2016-12-06 00:18:11 -0700

    Teststations updated with sde

--- evelib/objects/Station.py
+++ evelib/objects/Station.py
@@ -14,10 +14,31 @@ class Station(SqlBase, SqlObjectInterface):
     solar_system_id = Column(Integer, ForeignKey('solar_system.id'), nullable=False)
     r_solar_system = relationship("SolarSystem")
 
+    constellation_id = Column(Integer, ForeignKey('constellation.id'), nullable=False)
+    r_constellation = relationship("Constellation")
+
+    region_id = Column(Integer, ForeignKey('region.id'), nullable=False)
+    r_region = relationship("Region")
+
     @classmethod
     def create_new_object(cls, sql_session, crest_connection, id, name, solar_system_id, **kwargs):
         new_obj = cls(id=id, name=name)
         new_obj.solar_system_id = SolarSystem.get_from_db_or_crest_by_id(sql_session, crest_connection, solar_system_id).id
+        if 'write' in kwargs:
+            if kwargs['write']:
+                new_obj.write_to_db(sql_session)
+        return new_obj
+
+    @classmethod
+    def new_object_from_dict(cls, sql_session, crest_connection, entry, **kwargs):
+        solar_system_id = SolarSystem.get_from_db_or_crest_by_id(
+            sql_session, crest_connection, entry['solarSystemID']).id
+        new_obj = cls(id=entry['stationID'],
+                      name=entry['stationName'],
+                      solar_system_id=solar_system_id,
+                      constellation_id=entry['constellationID'],
+                      region_id=entry['regionID'],
+                      )
         if 'write' in kwargs:
             if kwargs['write']:
                 new_obj.write_to_db(sql_session)
