988fcead1ecd083197822a285eb3d24edbb1d308
Adam <adam@packer-debian-8-amd64.droplet.local>
2016-12-08 04:12:50 +0000

    Fixed a bug in setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -45,7 +45,7 @@ read -s PASSWORD
 mysql -u root -p"${PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
 mysql -u root -p"${PASSWORD}" -e "CREATE USER IF NOT EXISTS 'sql_user'@'localhost' IDENTIFIED BY '${key_sql_user}';"
 mysql -u root -p"${PASSWORD}" -e "GRANT CREATE, INSERT, SELECT ON ${DB_NAME}.* TO 'sql_user'@'localhost' IDENTIFIED BY '${key_sql_user}';"
-ql -u root -p"${PASSWORD}" -e "CREATE USER IF NOT EXISTS 'sql_remote_user'@'localhost' IDENTIFIED BY '${key_sql_remote_user}';"
+mysql -u root -p"${PASSWORD}" -e "CREATE USER IF NOT EXISTS 'sql_remote_user'@'localhost' IDENTIFIED BY '${key_sql_remote_user}';"
 mysql -u root -p"${PASSWORD}" -e "GRANT SELECT ON ${DB_NAME}.* TO 'sql_user'@'%' IDENTIFIED BY '${key_sql_remote_user}';"
 
 
